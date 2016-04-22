4e713822625e7b4caf295ee4ee33b9e03779a19a
andres-root <andres.root.coder@gmail.com>
2016-04-22 13:31:46 -0500

    update

--- www/views.py
+++ www/views.py
@@ -14,8 +14,8 @@ def index(request):
         tweets = Tweet.objects.all()
         tweet = tweets[len(tweets) - 1]
         tweet_object = {'name': tweet.name, 'user': tweet.user, 'text': tweet.text}
-        response = '*{0}\n{1}\n{2}'.format(tweet.name, tweet.user, tweet.text)
-        return HttpResponse(response)
-        # return JsonResponse(tweet_object, safe=False)
+        # response = '*{0}\n{1}\n{2}'.format(tweet.name, tweet.user, tweet.text)
+        # return HttpResponse(response)
+        return JsonResponse(tweet_object, safe=False)
     except Exception:
         return HttpResponse('DJANGO ERROR')
