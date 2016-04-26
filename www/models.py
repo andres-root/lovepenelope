0c04977bafe16dfbcf038a0c45b3dbafa268577a
andres-root <andres.root.coder@gmail.com>
2016-04-26 00:14:52 -0500

    update

--- www/models.py
+++ www/models.py
@@ -6,7 +6,9 @@ class Tweet(models.Model):
     name = models.CharField(max_length=200)
     user = models.CharField(max_length=200)
     text = models.CharField(max_length=140)
-    date = models.CharField(max_length=140)
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+    twitter_date_created = models.DateTimeField(blank=True)
+    date_updated = models.DateTimeField(auto_now=True, blank=True)
     active = models.BooleanField(default=True)
 
     def __str__(self):
