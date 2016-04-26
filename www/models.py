c650d7a799bb71542b41efe5d7d90f28fdacb38f
andres-root <andres.root.coder@gmail.com>
2016-04-25 23:57:04 -0500

    update

--- www/models.py
+++ www/models.py
@@ -1,14 +1,14 @@
 
 from django.db import models
-from datetime import datetime 
+from django.utils import timezone
 
 
 class Tweet(models.Model):
     name = models.CharField(max_length=200)
     user = models.CharField(max_length=200)
     text = models.CharField(max_length=140)
-    date_created = models.DateTimeField(auto_now_add=True, default=datetime.now())
-    twitter_date_created = models.DateTimeField(blank=False, null=False, default=datetime.now())
+    date_created = models.DateTimeField(auto_now_add=True, default=timezone.now())
+    twitter_date_created = models.DateTimeField(blank=False, null=False, default=timezone.now())
     date_updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
